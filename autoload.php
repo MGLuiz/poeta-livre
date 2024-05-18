fb289cc62c610102a06fbf2bec32deaeb73cfb30
Luiz Miguel Xavier de Brito <mgluiz1781@gmail.com>
2024-05-18 16:55:53 -0300

    script de class autoload adicionado

---
+++ autoload.php
@@ -0,0 +1,8 @@
+<?php
+    // class autoloader script
+    spl_autoload_register(function ($class){
+        if (file_exists($_SERVER['DOCUMENT_ROOT'] . '/poetalivre/assets/php/' .$class.'.php')){
+            require_once $_SERVER['DOCUMENT_ROOT'] .'/poetalivre/assets/php/' .$class.'.php';
+        }
+    });
+?>
