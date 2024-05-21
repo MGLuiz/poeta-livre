8c5f74f609244d789c4d3e0bec88deb7a2dc6c4f
Luiz Miguel Xavier de Brito <mgluiz1781@gmail.com>
2024-05-20 23:29:20 -0300

    autoloader atualizado

--- autoload.php
+++ autoload.php
@@ -3,6 +3,10 @@
     spl_autoload_register(function ($class){
         if (file_exists($_SERVER['DOCUMENT_ROOT'] . '/poetalivre/assets/php/' .$class.'.php')){
             require_once $_SERVER['DOCUMENT_ROOT'] .'/poetalivre/assets/php/' .$class.'.php';
+            
+        }else if (file_exists($_SERVER['DOCUMENT_ROOT'] . '/poetalivre/assets/php/DAO/' .$class.'.php')){
+            require_once $_SERVER['DOCUMENT_ROOT'] .'/poetalivre/assets/php/DAO/' .$class.'.php';
+
         }
     });
 ?>
